b0a3632f345d7eaed34232d1253f4701bb65483e
Brian Gikonyo <colasgikonyo@gmail.com>
2019-02-07 21:55:37 +0300

    more pages

--- users/templatetags/extra_tags.py
+++ users/templatetags/extra_tags.py
@@ -3,8 +3,16 @@ from rolepermissions.permissions import available_perm_status
 
 register = template.Library()
 
+def singular(value, arg):
+    """Replaces all values of arg from the given string with a space"""
+    if value == "repositories":
+        return "repository"
+    else:
+        return value[:-1]
+
 def cut(value, arg):
     """Replaces all values of arg from the given string with a space"""
-    return value[:-1]
+    return value.replace(arg, ' ')
 
+register.filter('singular', singular)
 register.filter('cut', cut)
